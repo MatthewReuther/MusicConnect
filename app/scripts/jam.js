8c2b80363ff641ccbabcd929ad846f1921acc715
Matthew Reuther <matthew.reuther@gmail.com>
2015-04-01 12:45:53 -0500

    now posting jams to firebase and returning an array

--- app/scripts/jam.js
+++ app/scripts/jam.js
@@ -1,7 +1,11 @@
 'Use Strict';
 
-app.controller('JamController', function($scope) {
+app.controller('JamController', function($scope, BASE, $firebase) {
+
+  var ref = new Firebase(BASE); //initizalies with firebase
+  var fbJams = $firebase(ref.child('jams')).$asArray(); //creates a jams then returns them as an array
+
   $scope.postJam = function(jam) {
-    console.log(jam);
+    fbJams.$add(jam);
   }
 });
