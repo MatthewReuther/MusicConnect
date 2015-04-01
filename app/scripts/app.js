99f46da63ae82a61df58ef68deec4eef3ef8c2dd
Matthew Reuther <matthew.reuther@gmail.com>
2015-04-01 11:28:56 -0500

    added views folder with  post edit browse and main.html, set up routes to each and added template to main.html

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,9 +1,24 @@
 'use strict';
 
 var app = angular
-  .module('MusicConnect', [
-    'ngAnimate',
-    'ngResource',
-    'ngRoute',
-    'firebase'
-  ])
+  .module('MusicConnect', ['ngRoute', 'ngAnimate', 'firebase'])
+
+  .constant('BASE', 'https://musicconnect.firebaseio.com/')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html'
+      })
+      .when('/post', {
+        templateUrl: 'views/post.html',
+      })
+      .when('/edit/:taskId', {
+        templateUrl: 'views/edit.html',
+      })
+      .when('/browse', {
+        templateUrl: 'views/browse.html',
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
